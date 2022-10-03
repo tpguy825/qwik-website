892bb97067ec34f46066d21b55792199f35cc6ba
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-10-03 18:39:18 +0100

    ssg

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Website made with qwik and vite.
 [Visit](https://tpguy825.cf)
 ## Old website (react)
 [Website](https://old.tpguy825.cf) - [Source](https://github.com/tpguy825/react-website)
+
+## Static Site Generator (Node.js)
+
+```
+npm run ssg
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig(() => {
   return {
+    ssr: { target: "node", format: "cjs" },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-	resolve: {
-		alias: {
-			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-		}
-	}
+    resolve: {
+      alias: {
+        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      },
+    },
   };
 });
