b0be0339058c59ba26eca6b16c407ecfdd3fe656
Toby <75158953+tpguy825@users.noreply.github.com>
2022-10-09 17:16:19 +0100

    go direct to express

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,28 @@
-import app from "../src/entry.express"
+import { qwikCity } from '@builder.io/qwik-city/middleware/node';
+import express from 'express';
+import { join } from 'path';
+import render from '../src/entry.ssr';
 
-export default app
+// Directories where the static assets are located
+const distDir = join(__dirname, '..', '..', 'dist');
+const buildDir = join(distDir, 'build');
+
+// Create the Qwik City express middleware
+const { router, notFound } = qwikCity(render);
+
+// Create the express server
+// https://expressjs.com/
+const app = express();
+
+// Static asset handlers
+// https://expressjs.com/en/starter/static-files.html
+app.use(`/build`, express.static(buildDir, { immutable: true, maxAge: '1y' }));
+app.use(express.static(distDir, { redirect: false }));
+
+// Use Qwik City's page and endpoint request handler
+app.use(router);
+
+// Use Qwik City's 404 handler
+app.use(notFound);
+
+module.exports = app
