65e3b0783b2c88d7fb5962bb380d5a857f00a0e1
Toby <75158953+tpguy825@users.noreply.github.com>
2022-10-09 17:02:40 +0100

    remove import.meta.url

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -1,11 +1,10 @@
 import { qwikCity } from '@builder.io/qwik-city/middleware/node';
 import express from 'express';
-import { fileURLToPath } from 'url';
 import { join } from 'path';
 import render from './entry.ssr';
 
 // Directories where the static assets are located
-const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
+const distDir = join(__dirname, '..', '..', 'dist');
 const buildDir = join(distDir, 'build');
 
 // Create the Qwik City express middleware
